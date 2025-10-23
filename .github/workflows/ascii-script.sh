af387bf59ee5528f150427821332fb93bfbe58ef
ChandraSekharc478 <chandrasekharc478@gmail.com>
2025-10-23 16:42:58 +0530

    Executing the job

---
+++ .github/workflows/ascii-script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Installing cowsay"
+sudo apt-get install cowsay -y
+cowsay -f dragon "Run for cover , I am a DRAGON...RAWR">> dragon.txt
+grep -i "dragon" dragon.txt
+cat dragon.txt
+ls -ltra
